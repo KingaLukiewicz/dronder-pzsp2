571694dd3ca1f1f4fbfd00fbd3fde88ab51c1bf1
Kinga <01187248@pw.edu.pl>
2025-11-29 20:30:10 +0100

    basic register page

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./page.module.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+export default function Register() {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [re_password, setRePassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  return (
+    <div className={styles.RegisterContainer}>
+      <div className={styles.RightBox} />
+      <div className={styles.RegisterLeft}>
+        <TextField
+          className={styles.Input}
+          label="imię"
+          variant="outlined"
+          value={first_name}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <TextField
+          className={styles.Input}
+          label="nazwisko"
+          variant="outlined"
+          value={last_name}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <TextField
+          className={styles.Input}
+          label="e-mail"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          className={styles.Input}
+          label="hasło"
+          variant="outlined"
+          type={"password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <TextField
+          className={styles.Input}
+          label="ponów hasło"
+          variant="outlined"
+          type={"password"}
+          value={re_password}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+        {error && <p className={styles.ErrorText}>{error}</p>}
+
+        <Button
+          className={styles.LeftButton}
+          sx={{ textTransform: "none !important" }}
+        >
+          Zarejestruj
+        </Button>
+      </div>
+
+      <div className={styles.RegisterRight}>
+        <h1>Witaj z powrotem!</h1>
+        <p>Masz już konto?</p>
+        <Button
+          className={styles.RightButton}
+          variant="outlined"
+          sx={{ textTransform: "none !important" }}
+        >
+          Zaloguj się!
+        </Button>
+      </div>
+    </div>
+  );
+}
