0ae2fe8aab4a2d60268cd26d1ab41226ee6bbce0
Filip Kopyt <filipkopyt14@gmail.com>
2025-11-29 18:58:50 +0100

    Added CORS handling

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 import auth
 from db import DATABASE_URL, db
@@ -8,6 +9,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.register_blueprint(auth.bp)
 db.init_app(app)
+_ = CORS(app)
 
 with app.app_context():
     db.create_all()
