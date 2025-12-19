8d6310972eaf47e95c9059eddcc96cb7cdedca71
Kinga <01187248@pw.edu.pl>
2025-12-19 23:34:59 +0100

    profile info

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import Header from "../header/page";
 import Sidebar from "../sidebar/page";
+import styles from "./page.module.css";
+import { Tooltip, Rating } from "@mui/material";
 
 export default function Profile() {
   const [sidebarVisible, setSidebarVisible] = useState(true);
@@ -11,9 +13,39 @@ export default function Profile() {
   };
 
   return (
-    <div className="Profile">
+    <div className={styles.Profile}>
       <Header toggleSidebar={toggleSidebar} />
       {sidebarVisible && <Sidebar />}
+      <main style={{ marginLeft: sidebarVisible ? "27vw" : "7vw" }}>
+        <h1>Mój profil</h1>
+        <div className={styles.InfoContainer}>
+          <div className={styles.Info}>
+            <h2>Jan Kowalski</h2>
+            <p>
+              Donec lobortis maximus erat non dapibus. Vivamus lobortis cursus
+              sodales. Phasellus fringilla arcu sit amet nunc placerat
+              fringilla. Aliquam interdum in odio nec sollicitudin. Quisque
+              rhoncus vel lacus ut suscipit. Donec auctor euismod lorem eu
+              luctus.
+            </p>
+            <div className={styles.Rating}>
+              <div className={styles.StarRating}>
+                <Tooltip placement="top" title={`4.56`}>
+                  <span>
+                    <Rating
+                      name="read-only"
+                      value={4.56}
+                      precision={0.1}
+                      readOnly
+                    />
+                  </span>
+                </Tooltip>
+              </div>
+              <p>{`84 oceny`}</p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
