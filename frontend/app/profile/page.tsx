8bd3efe9cfd6b69489fc8acb1b16233a6597855f
Kinga <01187248@pw.edu.pl>
2025-12-19 22:57:24 +0100

    basic profile with header and sidebar

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Header from "../header/page";
+import Sidebar from "../sidebar/page";
+
+export default function Profile() {
+  return (
+    <div className="Profile">
+      <Header />
+      <Sidebar />
+    </div>
+  );
+}
