58842371eaec84e34039e6d48e7a9ff316ae9f0c
Kinga <01187248@pw.edu.pl>
2025-12-20 23:12:09 +0100

    implemented profile form view

---
+++ frontend/app/profile_form/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+import styles from "./page.module.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { useState } from "react";
+
+export default function ProfileForm() {
+  const [aboutMe, setAboutMe] = useState("");
+  const [address, setAddress] = useState("");
+  const [range, setRange] = useState("");
+  const [state, setState] = useState({
+    map: false,
+    nmp: false,
+    nmpt: false,
+    cloud: false,
+    mesh: false,
+    scanning: false,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const { map, nmp, nmpt, cloud, mesh, scanning } = state;
+  return (
+    <div className={styles.Form}>
+      <h1>Uzupełnij swój profil</h1>
+      <main className={styles.MainContent}>
+        <h2>O mnie</h2>
+        <textarea
+          className={styles.AboutMe}
+          id="about-me"
+          rows={5}
+          value={aboutMe}
+          onChange={(e) => setAboutMe(e.target.value)}
+        />
+        <h2>Oferowane produkty</h2>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox checked={map} onChange={handleChange} name="map" />
+            }
+            label="Ortofotomapy"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={nmp} onChange={handleChange} name="nmp" />
+            }
+            label="Numeryczne Modele Terenu"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={nmpt} onChange={handleChange} name="nmpt" />
+            }
+            label="Numeryczne Modele Pokrycia Terenu"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={cloud} onChange={handleChange} name="cloud" />
+            }
+            label="Chmury Punktów"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={mesh} onChange={handleChange} name="mesh" />
+            }
+            label="Modele Mesh 3D"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={scanning}
+                onChange={handleChange}
+                name="scanning"
+              />
+            }
+            label="Scanning Laserowy"
+          />
+        </FormGroup>
+        <h2>Lokalizacja</h2>
+        <div className={styles.LocationRow}>
+          <label htmlFor="address">Adres:</label>
+          <input
+            className={styles.Input}
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <div className={styles.LocationRow}>
+          <label htmlFor="range">Zasięg (km):</label>
+          <input
+            className={styles.Input}
+            id="range"
+            value={range}
+            onChange={(e) => setRange(e.target.value)}
+          />
+        </div>
+        <div className={styles.ButtonRow}>
+          <Button
+            className={styles.Button}
+            sx={{ textTransform: "none !important" }}
+          >
+            Zapisz
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
+}
