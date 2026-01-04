c10f5cc24d363939cd22aa63bad491a593fbc688
Kinga <01187248@pw.edu.pl>
2026-01-04 19:54:43 +0100

    handle logout

--- frontend/app/header/page.tsx
+++ frontend/app/header/page.tsx
@@ -3,8 +3,16 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useRouter } from "next/navigation";
 
 const Header = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    sessionStorage.removeItem("token");
+    router.push("/login");
+  };
+
   return (
     <div className={styles.Header}>
       <div className={styles.HeaderLeft}>
@@ -24,7 +32,9 @@ const Header = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
       </div>
       <div className={styles.HeaderRight}>
         <NotificationsNoneOutlinedIcon sx={{ fontSize: "4vh" }} />
-        <LogoutOutlinedIcon sx={{ fontSize: "4vh" }} />
+        <div onClick={handleLogOut} style={{ cursor: "pointer" }}>
+          <LogoutOutlinedIcon sx={{ fontSize: "4vh" }} />
+        </div>
       </div>
     </div>
   );
