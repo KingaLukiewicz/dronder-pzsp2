93e0a1b75ac87a4d52b5718b0afb05683801d179
zuzawaszczuk <zuza.waszczuk@gmail.com>
2026-01-13 00:31:16 +0100

    without deleting match from db

--- backend/app/routes/matches.py
+++ backend/app/routes/matches.py
@@ -149,6 +149,5 @@ def decline_operator(offer_id: int, operator_id: int):
             return "", HTTPStatus.NOT_FOUND
 
         match.status = MatchingStatus.NOT_INTERESTED.value
-        session.delete(match)
         session.commit()
         return "", HTTPStatus.OK
