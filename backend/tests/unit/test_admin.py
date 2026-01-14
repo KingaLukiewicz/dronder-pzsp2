cb3865a7e861347467cdb29e3ba2d458b8058dc3
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2026-01-14 22:33:45 +0100

    fixed test for data change

--- backend/tests/unit/test_admin.py
+++ backend/tests/unit/test_admin.py
@@ -25,7 +25,7 @@ def test_admindata_endpoint(client: FlaskClient):
     assert response.json["number_of_admins"] == 1
     assert response.json["number_of_operators"] > 3
     assert response.json["number_of_clients"] > 3
-    assert response.json["operator_rating_stats"]["5"] == 4
+    assert response.json["operator_rating_stats"]["5"] == 5
     assert response.json["client_rating_stats"]["5"] == 4
     assert response.json["number_of_offers"] > 8
     assert isinstance(response.json["number_of_offers_by_deadline"], dict)
