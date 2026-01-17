4f1cc67e53f56f3cfe585c3929404b6762200146
KingaLukiewicz <kingal@itbs.pl>
2026-01-17 16:45:46 +0100

    added button to rate finalized order

--- frontend/app/my_orders/page.tsx
+++ frontend/app/my_orders/page.tsx
@@ -149,7 +149,9 @@ export default function MyOrders() {
                           ? new Date(offer.deadline_date).toLocaleDateString()
                           : "Brak terminu"
                       }
-                    />
+                    >
+                      <OrderPill.Rate />
+                    </OrderPill>
                   ))}
                 </div>
               ) : (
