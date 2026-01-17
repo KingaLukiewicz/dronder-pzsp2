243fdba479290518aa811d262b54e441c7b14fc4
Filip Kopyt <filipkopyt14@gmail.com>
2026-01-17 21:51:13 +0100

    map picker in progress

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
 import { redirect } from "next/navigation";
+import dynamic from "next/dynamic";
+
+const MapPicker = dynamic(() => import("./map_picker"), { ssr: false });
 
 export default function Home() {
+
+  return (
+    <MapPicker></MapPicker>
+  );
+
   redirect("/login");
 }
